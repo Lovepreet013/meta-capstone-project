a90d6a760bdc945aa164cb3c804535f5a0b8278f
Lovepreet013 <singhlovepreet1209@gmail.com>
2023-08-18 15:13:30 +0530

    open graph protocol added

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from "./icons_assets/Logo.svg"
+
+function Nav() {
+  return (
+    <nav>
+      <img src={logo} alt="" />
+      {/* <ul>
+        <li><a href="#"></a></li>
+      </ul> */}
+    </nav>
+  )
+}
+
+export default Nav
